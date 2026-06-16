6a0dcd5fe254906ae53bd49a55318bde537735be
Yukti <yuktikanojia19@gmail.com>
2026-06-16 20:37:17 +0530

    DAY-16 uploaded.

---
+++ DAY-16/Q63.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+//Q63) Write a program to Find pair with given sum. 
+int main() {
+
+    int n, i, j, sum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    // Input array
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
+
+    printf("Pairs are:\n");
+
+    // Find pairs
+    for(i = 0; i < n; i++) {
+
+        for(j = i + 1; j < n; j++) {
+
+            if(arr[i] + arr[j] == sum) {
+                printf("%d + %d = %d\n", arr[i], arr[j], sum);
+            }
+        }
+    }
+
+    return 0;
+}
