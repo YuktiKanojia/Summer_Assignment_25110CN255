3ef79f1e65f6cc6684e19991599696a0c038afa4
Yukti <yuktikanojia19@gmail.com>
2026-06-18 22:39:46 +0530

    DAY-18 uploaded.

---
+++ DAY-18/Q69.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//Q69) Write a program to Bubble sort. 
+int main() {
+    int n, i, j, temp;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - 1 - i; j++) {
+
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("Sorted Array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
